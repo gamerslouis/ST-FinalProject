726e44ea4db9b561f970905c03ae2a187115c486
gamerslouis <gamerslouis@gmail.com>
2022-05-06 15:04:54 +0800

    Make express serve html with canvas

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -1,9 +1,17 @@
 const supertest = require('supertest')
+const HTMLParser = require('node-html-parser')
 const { app } = require('./server')
 
-describe('Serve a web server', () => {
-  it('should get robots.txt', async () => {
+describe('a express server', () => {
+  it('should provide robots.txt', async () => {
     const res = await supertest(app).get('/robots.txt')
     expect(res.statusCode).toEqual(200)
-  })
+  }),
+    it('should provide index.html with a canvas whose id is canvas', async () => {
+      const res = await supertest(app).get('/')
+      expect(res.statusCode).toEqual(200)
+      expect(
+        HTMLParser.parse(res.text).querySelector('canvas#canvas')
+      ).toBeTruthy()
+    })
 })
