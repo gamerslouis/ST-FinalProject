fe035567556cc4b92088d6ed3e8068d0e405b8a2
gamerslouis <gamerslouis@gmail.com>
2022-05-06 16:03:57 +0800

    Fix typo error

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -6,12 +6,12 @@ describe('a express server', () => {
   it('should provide robots.txt', async () => {
     const res = await supertest(app).get('/robots.txt')
     expect(res.statusCode).toEqual(200)
-  }),
-    it('should provide index.html with a canvas whose id is canvas', async () => {
-      const res = await supertest(app).get('/')
-      expect(res.statusCode).toEqual(200)
-      expect(
-        HTMLParser.parse(res.text).querySelector('canvas#canvas')
-      ).toBeTruthy()
-    })
+  })
+  it('should provide index.html with a canvas whose id is canvas', async () => {
+    const res = await supertest(app).get('/')
+    expect(res.statusCode).toEqual(200)
+    expect(
+      HTMLParser.parse(res.text).querySelector('canvas#canvas')
+    ).toBeTruthy()
+  })
 })
