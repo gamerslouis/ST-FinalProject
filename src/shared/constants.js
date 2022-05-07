3135990995146d025b6bec9f3ab1a58829118855
yuko29 <yuko29.cs07@nctu.edu.tw>
2022-05-07 18:14:26 +0800

    Update constants.js

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,4 +1,11 @@
 module.exports = Object.freeze({
+  PLAYER_RADIOUS: 20,
+  PLAYER_MAX_HP: 100,
+  PLAYER_SPEED: 400,
+
+  BULLET_RADIUS: 3,
+  BULLET_SPEED: 1000,
+  BULLET_DAMAGE: 10,
   MSG_TYPES: {
     JOIN_GAME: 'join_game',
     INPUT: 'input',
