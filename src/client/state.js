768fb3669706105a3b75a1a6c5d0cfeca308593e
skysoul1024 <80204490+skysoul1024@users.noreply.github.com>
2022-05-12 12:55:49 +0800

    Add files via upload

--- src/client/state.js
+++ src/client/state.js
@@ -1,115 +1,95 @@
-const RENDER_DELAY = 100
+const RENDER_DELAY = 100;
 
-const gameUpdates = []
-let gameStart = 0
-let firstServerTimestamp = 0
+const gameUpdates = [];
+let gameStart = 0;
+let firstServerTimestamp = 0;
 
 export default class State {
   constructor() {
-    gameStart = 0
-    firstServerTimestamp = 0
+    this.gameStart = 0;
+    this.firstServerTimestamp = 0;
   }
 
   update(updateData) {
     this.updateData = updateData
     if (!firstServerTimestamp) {
-      firstServerTimestamp = this.updateData.GameUpdateMessage.t //這樣寫?
-      gameStart = Date.now()
+      firstServerTimestamp = this.updateData.GameUpdateMessage.t; //這樣寫?
+      gameStart = Date.now();
     }
-    gameUpdates.push(this.updateData)
+    gameUpdates.push(this.updateData);
 
-    const base = getBaseUpdate()
+    const base = getBaseUpdate();
     if (base > 0) {
-      gameUpdates.splice(0, base)
+      gameUpdates.splice(0, base);
     }
   }
 
   currentServerTime() {
-    return firstServerTimestamp + (Date.now() - gameStart) - RENDER_DELAY
+    return firstServerTimestamp + (Date.now() - gameStart) - RENDER_DELAY;
   }
 
   getBaseUpdate() {
-    const serverTime = currentServerTime()
+    const serverTime = currentServerTime();
     for (let i = gameUpdates.length - 1; i >= 0; i--) {
       if (gameUpdates[i].t <= serverTime) {
-        return i
+        return i;
       }
     }
-    return -1
+    return -1;
   }
 
   getCurrentState() {
     if (!firstServerTimestamp) {
-      return {}
+      return {};
     }
-
-    const base = getBaseUpdate()
-    const serverTime = currentServerTime()
+  
+    const base = getBaseUpdate();
+    const serverTime = currentServerTime();
 
     if (base < 0 || base === gameUpdates.length - 1) {
-      return gameUpdates[gameUpdates.length - 1]
+      return gameUpdates[gameUpdates.length - 1];
     } else {
-      const baseUpdate = gameUpdates[base]
-      const next = gameUpdates[base + 1]
-      const ratio =
-        (serverTime - baseUpdate.GameUpdateMessage.t) /
-        (next.GameUpdateMessage.t - baseUpdate.GameUpdateMessage.t) // t沒有傳
+      const baseUpdate = gameUpdates[base];
+      const next = gameUpdates[base + 1];
+      const ratio = (serverTime - baseUpdate.GameUpdateMessage.t) / (next.GameUpdateMessage.t - baseUpdate.GameUpdateMessage.t); // t沒有傳
       return {
         Loc: interpolateObject(baseUpdate.Loc, next.Loc, ratio),
-        Airplane: interpolateObjectArray(
-          baseUpdate.Airplane,
-          next.Airplane,
-          ratio
-        ),
-        GameUpdateMessage: interpolateObjectArray(
-          baseUpdate.GameUpdateMessage,
-          next.GameUpdateMessage,
-          ratio
-        ),
-      }
+        Airplane: interpolateObjectArray(baseUpdate.Airplane, next.Airplane, ratio),
+        GameUpdateMessage: interpolateObjectArray(baseUpdate.GameUpdateMessage, next.GameUpdateMessage, ratio),
+      };
     }
   }
 
   interpolateObject(object1, object2, ratio) {
     if (!object2) {
-      return object1
+      return object1;
     }
-
-    const interpolated = {}
-    Object.keys(object1).forEach((key) => {
+  
+    const interpolated = {};
+    Object.keys(object1).forEach(key => {
       if (key === 'direction') {
-        interpolated[key] = interpolateDirection(
-          object1[key],
-          object2[key],
-          ratio
-        )
+        interpolated[key] = interpolateDirection(object1[key], object2[key], ratio);
       } else {
-        interpolated[key] = object1[key] + (object2[key] - object1[key]) * ratio
+        interpolated[key] = object1[key] + (object2[key] - object1[key]) * ratio;
       }
-    })
-    return interpolated
+    });
+    return interpolated;
   }
-
+  
   interpolateObjectArray(objects1, objects2, ratio) {
-    return objects1.map((o) =>
-      interpolateObject(
-        o,
-        objects2.find((o2) => o.id === o2.id),
-        ratio
-      )
-    )
+    return objects1.map(o => interpolateObject(o, objects2.find(o2 => o.id === o2.id), ratio));
   }
-
+  
   interpolateDirection(d1, d2, ratio) {
-    const absD = Math.abs(d2 - d1)
+    const absD = Math.abs(d2 - d1);
     if (absD >= Math.PI) {
       if (d1 > d2) {
-        return d1 + (d2 + 2 * Math.PI - d1) * ratio
+        return d1 + (d2 + 2 * Math.PI - d1) * ratio;
       } else {
-        return d1 - (d2 - 2 * Math.PI - d1) * ratio
+        return d1 - (d2 - 2 * Math.PI - d1) * ratio;
       }
     } else {
-      return d1 + (d2 - d1) * ratio
+      return d1 + (d2 - d1) * ratio;
     }
   }
 }
