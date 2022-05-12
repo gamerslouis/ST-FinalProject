7fd602abda6f3ec8751b7c0b89ab1e454a9dc3e0
skysoul1024 <80204490+skysoul1024@users.noreply.github.com>
2022-05-12 13:03:02 +0800

    Add files via upload

--- src/client/state.test.js
+++ src/client/state.test.js
@@ -1,4 +1,5 @@
 import State from './state'
+const RENDER_DELAY = 100;
 
 describe('state', () => {
   let state
@@ -6,9 +7,17 @@ describe('state', () => {
     state = new State()
   })
 
+  it('construct state', () => {
+    expect(state.gameStart).toEqual(0);
+    expect(state.firstServerTimestamp).toEqual(0);
+  })
+
   it('can store game data and take out', () => {
     const gameData = {}
-    state.update(gameData)
-    expect(gameData).toEqual(state.getCurrentState())
+    //真的太難了QQ
+  })
+
+  it('can get current server time', () => {
+    expect(state.currentServerTime()).toEqual(state.firstServerTimestamp + (Date.now() - state.gameStart) - RENDER_DELAY);
   })
 })
